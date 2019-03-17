beef380921b80aca8b772d150873e0b341876494
vikvik98 <vinicius.c.mascarenhas@hotmail.com>
2019-03-17 19:52:43 -0300

    Brabo

---
+++ introducaots/app/ts/app.js
@@ -0,0 +1,6 @@
+function soma(x, y) {
+    return x + y;
+}
+var a = soma;
+console.log(a(2, 2));
+//# sourceMappingURL=app.js.map
---
+++ introducaots/app/ts/app.ts
@@ -0,0 +1,8 @@
+
+function soma(x: number, y: number): number{
+    return x+y
+}
+
+var a = soma;
+
+console.log(a(2,2));
