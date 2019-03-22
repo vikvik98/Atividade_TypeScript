ef03b3226fe6a98fb779779428a6bf1f0e50f1de
Meu Nome <danielavieira6689@gmail.com>
2019-03-22 18:02:05 -0300

    Atualizacao

--- introducaots/app/ts/app.js
+++ introducaots/app/ts/app.js
@@ -1,6 +1,32 @@
+//Primeira questao
+//A:
+// let a = 10;
+// a = "2"
+//B:
+var b = 10;
+b = 2;
+//C:
+var c = 10;
+c = 2;
+// Resposta: A letra a não roda porque a variavel foi tipada como number e uma vez que uma variavel no typescript ela não pode ser trocada de tipo;
+//Segunda questão
 function soma(x, y) {
     return x + y;
 }
-var a = soma;
-console.log(a(2, 2));
+//A:
+console.log(soma(1, 2));
+//B:
+console.log(soma(1, "2"));
+//C:
+console.log(soma(1));
+//RESPOSTA: A LETRA C NÃO FUNCIONA PQ A FUNÇÃO ESPERAVA COMO PARAMETRO 2 NUMEROS;
+//TERCEIRA
+var Estados;
+(function (Estados) {
+    Estados[Estados["PI"] = 0] = "PI";
+    Estados[Estados["CE"] = 1] = "CE";
+    Estados[Estados["MA"] = 2] = "MA";
+})(Estados || (Estados = {}));
+for (var i = 0; i < Object.keys(Estados).length / 2; i++)
+    console.log(Estados[i]);
 //# sourceMappingURL=app.js.map
--- introducaots/app/ts/app.ts
+++ introducaots/app/ts/app.ts
@@ -1,8 +1,44 @@
+//Primeira questao
 
-function soma(x: number, y: number): number{
-    return x+y
+//A:
+// let a = 10;
+// a = "2"
+
+//B:
+
+let b: any = 10;
+b = 2
+
+//C:
+
+let c:number = 10;
+c = 2;
+
+// Resposta: A letra a não roda porque a variavel foi tipada como number e uma vez que uma variavel no typescript ela não pode ser trocada de tipo;
+
+
+//Segunda questão
+
+function soma(x: number, y?:any):number{
+
+    return x + y
 }
 
-var a = soma;
+//A:
+console.log(soma(1,2));
 
-console.log(a(2,2));
+//B:
+console.log(soma(1,"2"));
+
+//C:
+console.log(soma(1));
+
+//RESPOSTA: A LETRA C NÃO FUNCIONA PQ A FUNÇÃO ESPERAVA COMO PARAMETRO 2 NUMEROS;
+
+
+//TERCEIRA
+
+enum Estados {"PI", "CE", "MA"}
+
+for( let i = 0; i < Object.keys(Estados).length / 2; i++)
+    console.log(Estados[i])
